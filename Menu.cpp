38010f105614c805531ce0527183ceb7a8572875
Kai Uerlichs <2421101@dundee.ac.uk>
2021-11-14 23:26:29 +0000

    Added some comments and bug fixes and stuff

--- Menu.cpp
+++ Menu.cpp
@@ -1,18 +1,42 @@
+/**
+ * FILE: Menu.cpp
+ * 
+ * GROUP: C
+ * Ross Coombs
+ * Elliot Scott
+ * Kai Uerlichs
+ * 
+ * This file include the implementation for classes and members outlined in Automaton.h
+ */
+
+
+
+// Required std includes and namespace setting
 #include "Menu.h"
 #include "Automaton.h"
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+/**
+ * @brief This is the main method of the Automaton program
+ * 
+ * @return int Success code
+ */
 int main()
 {
     Menu menu;
-    menu.displayMainMenu();
+    // menu.displayMainMenu();
+    cout << menu.createRandomSeed(8);
 }
 
+/**
+ * @brief This method displays the main menu
+ */
 void Menu::displayMainMenu(){
     int userChoice;
     do{
-        std::cout<<"[0] Quit\n[1] Run 1D Program\n[2] Use Convertersn";
+        std::cout<<"[0] Quit\n[1] Run 1D Program\n[2] Use Converters\n";
         userChoice = getUserChoice(0, 1);
         switch(userChoice) {
             case 0:
@@ -82,36 +106,54 @@ void Menu::displayMainMenu(){
                 std::cout<<"invalid";
                 break;
         }
-    }while(userChoice!=0);
+    } while(userChoice!=0);
 }
 
-
-
+/**
+ * @brief Method prompts a user choice and returns the specified input
+ * 
+ * @param lowerBound The lower bound for the user input method
+ * @param upperBound The upper bound for the user input method
+ * @return int The user-specified input
+ */
 int Menu::getUserChoice(int lowerBound, int upperBound){
-    int userChoice;
-    bool valid = true;
+    string userChoice;
+    int numericChoice;
+    bool valid = false;
 
-    while (valid)
+    while (!valid)
     {
-        // gets user input
-        cout <<"please enter a number between: "<< lowerBound << "and "<< upperBound<< ": ";
+        // Gets user input
+        cout << "Enter a number between " << lowerBound << " and " << upperBound << ": ";
         cin >> userChoice;
-        //input validation
-        if (userChoice >= lowerBound && userChoice <= upperBound)
-        {
-            valid = false;
+        // Input validation
+        try{
+            numericChoice = stoi(userChoice);
+        }
+        catch(exception& e){
+            cout<< "Your input was invalid. Try again."<< endl;
+            continue;
+        }
+        if (numericChoice >= lowerBound && numericChoice <= upperBound){
+            valid = true;
         }
         else{
-            cout<< "please enter a valid choice"<< endl;
+            cout<< "Your input was invalid. Try again."<< endl;
         }
-
     }
 
-    //returns value
-    return userChoice;
+    // Returns value
+    return numericChoice;
 
 }
 
+/**
+ * @brief Converts a decimal number to string representation of the decimal number
+ * 
+ * @param number The number to convert
+ * @param numOfBits The number of bits
+ * @return string Returns the binary as string
+ */
 string Menu::convertToBinary(int number, int numOfBits){
     //initialise arrays
     int remainderArray[numOfBits];
@@ -135,7 +177,14 @@ string Menu::convertToBinary(int number, int numOfBits){
     return binaryArray;
 }
 
-int Menu::convertToDecimal(char number[], int numOfBits){
+/**
+ * @brief Converts a binary number into decimal
+ * 
+ * @param number The binary to convert
+ * @param numOfBits The number of bits
+ * @return int The decimal number
+ */
+int Menu::convertToDecimal(string number, int numOfBits){
     
     //initialise values
     int counter = 0;
@@ -151,18 +200,25 @@ int Menu::convertToDecimal(char number[], int numOfBits){
     return decimal;
 }
 
+/**
+ * @brief Create a random seed for the 1D array
+ * 
+ * @param Size The size for the random seed
+ * @return string The random seed
+ */
 string Menu::createRandomSeed(int Size){
-   //initialise variables
-    
+    // Set random seed to time value
+    srand(time(0));
+
+   // Initialise variables
     int loop = 0;
     int randomNumber;
     char seed[Size];
     int iteration;
-    // get random number between 0 and 256
-    // turn intop binary and return it
-   do
-   {
-       //gets random number from 0 to randsize
+    
+    do
+    {
+        //gets random number from 0 to randsize
         randomNumber = (rand() % Size);
         //sets the array's data node for the random number to #
         seed[randomNumber] = '1';
@@ -170,16 +226,19 @@ string Menu::createRandomSeed(int Size){
         iteration++;
 
     // checks if the loop should randomly end or if the array is full
-   }while (loop != 1 || iteration >= Size);
+    } while (loop != 1 || iteration <= Size);
 
-   for (int i = 0; i < Size; i++)
-   {
-       if (seed[i] != '1')
-       {
-           seed[i] = '0';
-       }
-       
-   }
+    // Set all non-positive values to zero
+    for (int i = 0; i < Size; i++)
+    {
+        if (seed[i] != '1')
+        {
+            seed[i] = '0';
+        }
+        
+    }
+
+    // Return the seed
     return seed;
 }
 
--- Menu.h
+++ Menu.h
@@ -1,16 +1,39 @@
+/**
+ * FILE: Menu.h
+ * 
+ * GROUP: C
+ * Ross Coombs
+ * Elliot Scott
+ * Kai Uerlichs
+ * 
+ * This file defines the Menu class and its members
+ */
+
+
+
+// Beginning of header guard
 #ifndef MENU_H
 #define MENU_H
 
+
+
+// Required includes and namespace setting
 #include <string>
 using namespace std;
 
+/**
+ * @brief The Menu class controls the main control flow of the program
+ */
 class Menu{
     public:
         void displayMainMenu();
         int getUserChoice(int lowerBound, int upperBound);   
         string convertToBinary(int number, int numOfBits);
-        int convertToDecimal(char number[], int numOfBits);
+        int convertToDecimal(string number, int numOfBits);
         string createRandomSeed(int size);
 };
 
+
+
+// End of header guard
 #endif
