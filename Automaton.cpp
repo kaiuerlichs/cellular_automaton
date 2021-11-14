17bf54121510ccce4add08134219ae35c287063c
Kai Uerlichs <2421101@dundee.ac.uk>
2021-11-14 16:05:41 +0000

    Lots of changes probs only 2D automaton and comments tho

--- Automaton.cpp
+++ Automaton.cpp
@@ -1,8 +1,35 @@
+/**
+ * FILE: Automaton.cpp
+ * 
+ * GROUP: C
+ * Ross Coombs
+ * Elliot Scott
+ * Kai Uerlichs
+ * 
+ * This file include the implementation for classes and members outlined in Automaton.h
+ */
+
+
+
+// Required std includes and namespace setting
 #include "Automaton.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-// Automaton1D
+
+
+/* AUTOMATON 1D IMPLEMENTATIONS */
+
+/**
+ * @brief Construct a new Automaton1D object
+ * 
+ * @param width The width of the 1D automaton
+ * @param numberOfIterations The number of iterations (generations) to generate
+ * @param seed The start value for the automaton
+ * @param rule The ruleset by which a cell's value is decided each generation
+ * @param wrap Whether the ends of the automaton are connected (true) or separated (false)
+ */
 Automaton1D::Automaton1D(int width, int numberOfIterations, string seed, string rule, bool wrap) {
     // Validate input parameters
     if(seed.length() <= 0 || width <= 0 || numberOfIterations <= 0 || rule.length() <= 0){
@@ -27,6 +54,9 @@ Automaton1D::Automaton1D(int width, int numberOfIterations, string seed, string 
     iter.assign(numberOfIterations+1, vector<char> (width, '-'));
 }
 
+/**
+ * @brief Runs the 1D Automaton's generation based on the parameters specified at object creation
+ */
 void Automaton1D::runAutomaton(){
     // Load first generation (seed) into vector matrix
     for(int i = 0; i < width; i++){
@@ -36,6 +66,7 @@ void Automaton1D::runAutomaton(){
     // Generate remaining generations
     for(int i = 1; i <= numberOfIterations; i++){
         for(int j = 0; j < width; j++){
+            // Check state of left, center and right cell in previous generation
             char lstate;
             char cstate;
             char rstate;
@@ -59,6 +90,7 @@ void Automaton1D::runAutomaton(){
                 rstate = iter[i-1][j+1];
             }
 
+            // For each bit of the ruleset, check if it is the correct setting and apply the rule
             if(lstate == '1' && cstate == '1' && rstate == '1'){
                 iter[i][j] = rule.at(0);
             }
@@ -86,8 +118,10 @@ void Automaton1D::runAutomaton(){
         }
     }
 
+    // Set the generation value to true
     generationDone = true;
 
+    // Pretty-print the automaton to the console
     for(vector<char> v : iter){
         for (char c : v){
             if(c == '1'){
@@ -101,16 +135,58 @@ void Automaton1D::runAutomaton(){
     }
 }
 
+/**
+ * @brief Saves the 1D Automaton's output to a file
+ * 
+ * @param filename The file to save the automaton to
+ */
 void Automaton1D::saveAutomaton(string filename){
-    return;
+    // Check if automaton was created
+    if(!generationDone){
+        throw logic_error("Automaton must run before saving it");
+    }
+
+    // Check if file already exists
+    ifstream in;
+    in.open(filename);
+    if(in){
+        throw invalid_argument("File already exists");
+    }
+    
+    // Attempt to create new file to write into
+    in.close();
+    ofstream out;
+    out.open(filename);
+    // Throw exception if file creation was not possible
+    if(!out){
+        throw runtime_error("File creation failed");
+    }
+
+    // Write to file
+    for(vector<char> v : iter){
+        for (char c : v){
+            out << c << " ";
+        }
+        out << endl;
+    }
+
+    // Close output stream
+    out.close();
 }
 
+/**
+ * @brief Saves the Automaton's generation settings as a preset, so the user can run the same automaton again
+ * 
+ * @param presetName The name to store the preset under
+ */
 void Automaton1D::saveAsPreset(string presetName){
     return;
 }
 
-int main(){
-    Automaton1D a1(31, 15, "0000000000000001000000000000000", "00011110" ,false);
-    a1.runAutomaton();
-    return 0;
+
+
+/* AUTOMATON 2D IMPLEMENTATIONS */
+
+Automaton2D::Automaton2D(int width, int height, int numberOfIterations, string seed, bool wrap){
+    
 }
